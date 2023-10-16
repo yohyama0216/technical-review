ea36c9c1081564b30ab163fa7562a8b22b0105e1
yohyama <yohyama0216>
2023-10-17 00:09:41 +0900

    テストメソッド漏れ検出テスト、routeNameとpathのテストを実装

---
+++ src/database/factories/QuestionFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    protected $model = Question::class;
+
+    public function definition()
+    {
+        return [
+            'question' => $this->faker->sentence(),
+            // ... その他の属性
+        ];
+    }
+}
